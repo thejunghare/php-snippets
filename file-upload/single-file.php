3785b1cfb9bfb27ea6f0674203caf1a0ac24342b
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-10-09 12:46:50 +0530

    added single-file.php

---
+++ file-upload/single-file.php
@@ -0,0 +1,15 @@
+ $filename = basename($_FILES['file']['name']);
+    $tempFileName = $_FILES['file']['tmp_name'];
+    $upload_dir = '../pdf/navbar-attachment/';
+    $error = null;
+
+    if ($error == UPLOAD_ERR_OK) {
+        if (move_uploaded_file($tempFileName, "$upload_dir/$filename")) {
+            echo "<script>alert('Moved :)')</script>";
+            /*
+        $sql = "INSERT INTO users (name, surname, sex) VALUES (?,?,?)";
+        $pdo->prepare($sql)->execute([$name, $surname, $sex]);
+        */
+
+        }
+    }
