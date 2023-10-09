8ed7db913bedcfe54b2693f07b4e1486e80d28d8
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-10-09 14:49:09 +0530

    Update single-file.php

--- file-upload/single-file.php
+++ file-upload/single-file.php
@@ -1,13 +1,17 @@
 <?php
 $filename = basename($_FILES["file"]["name"]);
 $filename = str_replace(" ", "_", $filename); # replace space with '_'
+
 $tempFileName = $_FILES["file"]["tmp_name"];
 $upload_dir = "./pdf/";
-$error = null;
+
+$error = $_FILES['file']['error']; # check error status
+
 # check if folder exist
 if (!file_exists($upload_dir)) {
     mkdir("./pdf/", 0777, true);
 }
+
 
 if ($error == UPLOAD_ERR_OK) {
     if (move_uploaded_file($tempFileName, "$upload_dir/$filename")) {
