86bfef008e41050fa13d75c0b710684c7fdd1638
thejunghare <prasadjunghare2@gmail.com>
2023-12-24 14:31:19 +0530

    add construct.php destruct

--- oops/construct.php
+++ oops/construct.php
@@ -1,30 +1,51 @@
 <?php
 /* 
-A constructor allows you to initialize an object's properties upon creation of the object.
+    A constructor allows you to initialize an object's properties upon creation of the object.
 
-If you create a __construct() function, PHP will automatically call this function when you create an object from a class.
+    If you create a __construct() function, PHP will automatically call this function when you create an object from a class.
 
-Notice that the construct function starts with two underscores (__)!
+    Notice that the construct function starts with two underscores (__)!
 */
-class Car{
+class Car
+{
     public $name;
     public $color;
 
-    function __construct($name,$color){
+    function __construct($name, $color)
+    {
         $this->name = $name;
         $this->color = $color;
     }
 
-    function getname (){
+    function getname()
+    {
         return $this->name;
     }
 
-    function getcolor (){
+    function getcolor()
+    {
         return $this->color;
+    }
+
+    /* 
+        A destructor is called when the object is destructed or the script is stopped or exited.
+
+        If you create a __destruct() function, PHP will automatically call this function at the end of the script.
+    */
+
+    function __destruct()
+    {
+        # echo $this->name . "\n";
+        # echo $this->color . "\n";
+        # OR
+        echo "The fruit is {$this->name} and the color is {$this->color}.";
     }
 }
 
 $bmw = new Car('BMW', 'White');
-echo $bmw->getname(). "\n";
-echo $bmw->getcolor(). "\n";
+/*
+    # This statements are not required if used destructor 
+    echo $bmw->getname() . "\n";
+    echo $bmw->getcolor() . "\n"; 
+*/
 ?>
