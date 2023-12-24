58695ff46ea983338844bf85995ca1c647897e44
thejunghare <prasadjunghare2@gmail.com>
2023-12-24 13:56:25 +0530

    added what is oop?

---
+++ oops/whatIsOops.php
@@ -0,0 +1,12 @@
+<?php 
+/* 
+
+OOP -> object oriented programming.
+is about creating objects that contain both data and functions
+
+Classes and Objects
+Class -> Fruit
+Objects -> Apple, Banana, Mango, etc
+
+*/
+?>
